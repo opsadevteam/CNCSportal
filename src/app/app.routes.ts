7ce2adc4c982956e9fe23032a08fb9fc02831ed1
arvi811 <robertverano11895@gmail.com>
2024-11-18 14:02:30 +0800

    add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,49 @@
 import { Routes } from '@angular/router';
+import { PhoneEntryFormComponent } from './components/EntryForms/phone-entry-form/phone-entry-form.component';
+import { LoginComponent } from './components/login/login.component';
+import { EmailEntryFormComponent } from './components/EntryForms/email-entry-form/email-entry-form.component';
+import { PhoneRecordsComponent } from './components/Records/phone-records/phone-records.component';
+import { EmailRecordsComponent } from './components/Records/email-records/email-records.component';
+import { UserManagementComponent } from './components/Records/user-management/user-management.component';
+import { ActivityLogsComponent } from './components/Records/activity-logs/activity-logs.component';
+import { WorkloadStatisticsComponent } from './components/Reports/workload-statistics/workload-statistics.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'phoneform',
+    component: PhoneEntryFormComponent,
+  },
+  {
+    path: 'emailform',
+    component: EmailEntryFormComponent,
+  },
+  {
+    path: 'phonerecords',
+    component: PhoneRecordsComponent,
+  },
+  {
+    path: 'emailrecords',
+    component: EmailRecordsComponent,
+  },
+  {
+    path: 'usermanagement',
+    component: UserManagementComponent,
+  },
+  {
+    path: 'activitylogs',
+    component: ActivityLogsComponent,
+  },
+  {
+    path: 'workloadstatistics',
+    component: WorkloadStatisticsComponent,
+  },
+];
