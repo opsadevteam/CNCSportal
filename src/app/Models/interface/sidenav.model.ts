dc8cbe21980a6cd69a6472664ea7763d8df3db0a
arvi811 <robertverano11895@gmail.com>
2024-11-18 13:34:33 +0800

    add models

---
+++ src/app/Models/interface/sidenav.model.ts
@@ -0,0 +1,24 @@
+export interface IEntryFormsMenuItem {
+    icon: string;
+    label: string;
+    route?: string;
+  }
+  
+  export interface IRecordsMenuItem {
+    icon: string;
+    label: string;
+    route?: string;
+  }
+  
+  export interface IReportsMenuItem {
+    icon: string;
+    label: string;
+    route?: string;
+  }
+  
+  export interface IAccountsMenuItem {
+    icon: string;
+    label: string;
+    route?: string;
+  }
+  //2
