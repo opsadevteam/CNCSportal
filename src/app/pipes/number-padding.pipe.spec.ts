ccadf1924dfb93d44309fd78c62cf052603da4f0
laur021 <laurence.bacolor@gmail.com>
2024-11-22 15:16:14 +0800

    fix files

--- src/app/pipes/number-padding.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NumberPaddingPipe } from './number-padding.pipe';
-
-describe('NumberPaddingPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NumberPaddingPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
