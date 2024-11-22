58b9bd5211c4453a7cdf9e3be5be3c8c25700bd0
laur021 <laurence.bacolor@gmail.com>
2024-11-22 11:57:00 +0800

    removed cached file in git

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  API_LOCAL_CNCS_URL: "http://localhost:5001/api/",
-  API_CNCS_URL: "https://cryptotracker.bestresource-inc.com/api/v1/",
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  API_LOCAL_EMPLOYEE_URL: "http://localhost:5001/api/",
-  API_EMPLOYEE_URL: "https://cryptotracker.bestresource-inc.com/api/v1/",
-};
