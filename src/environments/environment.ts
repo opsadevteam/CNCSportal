431c91b5d096a54ac565610cb4f4c6e9eb99e7db
laur021 <laurence.bacolor@gmail.com>
2024-11-25 13:10:22 +0800

    edit environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   ENVI_POINT: "LOCAL",
-  LOCAL: "http://localhost:7050/api/v1/",
+  LOCAL: "https://localhost:7050/api/v1/",
   DEV: "https://webapi.bestresource-inc.com/api/v1/",
   STAGE: "https://webapi.bestresource-inc.com/api/v1/",
   PROD: "https://webapi.bestresource-inc.com/api/v1/",
