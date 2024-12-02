9e510bdaabb3a957325a3abdad8ac305e8cc9cb9
arvi811 <robertverano11895@gmail.com>
2024-12-02 16:03:13 +0800

    add autocomplete Suggeston on Description

--- src/app/components/EntryForms/phone-entry-form/phone-entry-form.component.ts
+++ src/app/components/EntryForms/phone-entry-form/phone-entry-form.component.ts
@@ -32,11 +32,15 @@ import {
   MatDialogModule,
   MatDialogRef,
 } from '@angular/material/dialog';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { filter, map, observable, Observable, startWith } from 'rxjs';
+import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
 
 @Component({
   selector: 'app-phone-entry-form',
   standalone: true,
   imports: [
+    MatAutocompleteModule,
     CommonModule,
     MatDialogActions,
     MatDialogClose,
@@ -66,6 +70,7 @@ export class PhoneEntryFormComponent implements OnInit {
   descriptionList: IDescription[] = [];
   userAccountList: IUserAccount[] = [];
   router: any;
+  filteredOption: Observable<IDescription[]> = new Observable<IDescription[]>();
 
   constructor(
     private fb: FormBuilder,
@@ -80,17 +85,28 @@ export class PhoneEntryFormComponent implements OnInit {
   }
 
   initForm() {
-    this.phoneEntryForm = this.fb.group({
-      phoneId: ['', [Validators.required]],
-      customerId: ['', [Validators.required]],
-      pickUpDate: ['', [Validators.required]],
-      takeOffDate: ['', [Validators.required]],
-      productVendor: ['', [Validators.required]],
-      description: ['', [Validators.required]],
-      remark: ['', [Validators.required]],
-      repliedBy: ['', [Validators.required]],
-      status: ['', [Validators.required]],
+    this.phoneEntryForm = new FormGroup({
+      phoneId: new FormControl('', [Validators.required]),
+      customerId: new FormControl('', [Validators.required]),
+      pickUpDate: new FormControl('', [Validators.required]),
+      takeOffDate: new FormControl('', [Validators.required]),
+      productVendor: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      remark: new FormControl('', [Validators.required]),
+      repliedBy: new FormControl('', [Validators.required]),
+      status: new FormControl('', [Validators.required]),
     });
+    this.filteredOption = this.phoneEntryForm.valueChanges.pipe(
+      startWith(this.phoneEntryForm.value),
+      map((value) => this._filter(value.description))
+    );
+  }
+
+  private _filter(value: string) {
+    const filtervalue = value.toLowerCase();
+    return this.descriptionList.filter((option) =>
+      option.description.toLowerCase().includes(filtervalue)
+    );
   }
 
   getAllProductVendors() {
