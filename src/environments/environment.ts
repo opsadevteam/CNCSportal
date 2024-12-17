f3689a22b9d968e407a7972df504c2feec47bd01
arvi811 <robertverano11895@gmail.com>
2024-12-17 14:01:43 +0800

    add new offical url envi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-   // dynamic pointing of environment
+  // dynamic pointing of environment
   ENVI_POINT: "LOCAL",
   LOCAL: "https://localhost:7050/api/v1/",
-  DEV: "https://webapi.bestresource-inc.com/api/v1/",
-  STAGE: "https://webapi.bestresource-inc.com/api/v1/",
-  PROD: "https://webapi.bestresource-inc.com/api/v1/",
+  DEV: "https://cncswebapi-dev.bestresource-inc.com/api/v1/",
+  STAGE: "https://cncswebapi-stage.bestresource-inc.com/api/v1/",
+  PROD: "https://cncswebapi.bestresource-inc.com/api/v1/",
 };
 
